d0ac910dbcfd92923b9f3ac0e843510e61e679fe
TrusTio <erkan.myumyunov@gmail.com>
2025-01-28 12:13:50 +0200

    Completed Stage 4/7

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -5,22 +5,34 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-// Completed Stage 3/7  - https://hyperskill.org/projects/53/stages/289/implement
+// Completed Stage 4/7  - https://hyperskill.org/projects/53/stages/290/implement
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int codeLength = scanner.nextInt();
-        if (codeLength > 10) {
-            System.out.println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.\n");
-        } else {
-            String secretCode = randomCodeGenerator(codeLength);
-            System.out.println("The random secret number is " + secretCode + ".");
-        }
+        String secretCode = "";
 
-        //String secretCode = "9305";
-        //System.out.println("The secret code is prepared: ****.");
-        //String guesserInput = scanner.nextLine();
-        //System.out.println(checkForCowsAndBulls(secretCode, guesserInput) + " The secret code is 9305.");
+        do {
+            System.out.println("Please, enter the secret code's length:");
+            int codeLength = scanner.nextInt();
+            scanner.nextLine();
+            if (codeLength > 10) {
+                System.out.println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.");
+            } else {
+                secretCode = randomCodeGenerator(codeLength);
+                System.out.println("Okay, let's start a game!");
+            }
+        } while (secretCode.isEmpty());
+
+
+        String guesserInput;
+        int turn = 1;
+        do {
+            System.out.println("Turn " + turn + ":");
+            guesserInput = scanner.nextLine();
+            System.out.println(checkForCowsAndBulls(secretCode, guesserInput));
+            turn++;
+        } while (!guesserInput.equals(secretCode));
+        System.out.println("Congratulations! You guessed the secret code.");
     }
 
     // Generates a random Secret Code with no repeating numbers using a shuffled list.
@@ -43,15 +55,9 @@ public class Main {
     public static String checkForCowsAndBulls(String secretCode, String guesserInput) {
         int cows = 0;
         int bulls = 0;
-        int[] secretCodeArray = secretCode
-                .chars()
-                .map(Character::getNumericValue)
-                .toArray();
+        int[] secretCodeArray = secretCode.chars().map(Character::getNumericValue).toArray();
 
-        int[] guesserInputArray = guesserInput
-                .chars()
-                .map(Character::getNumericValue)
-                .toArray();
+        int[] guesserInputArray = guesserInput.chars().map(Character::getNumericValue).toArray();
 
         for (int i = 0; i < secretCodeArray.length; i++) {
             for (int j = 0; j < guesserInputArray.length; j++) {
